dbed5cdb01e851232fb36352f5dc2bd9bcd4c66c
DESKTOP-NQHV1GS\admin <mishakovalevst@gmail.com>
2023-03-15 16:33:37 +0300

    дискрим

--- lekzii/#6.php
+++ lekzii/#6.php
@@ -1,13 +1,16 @@
 <?php
 $a = 1;
-$b = -1;
+$b = 30;
 $c = 1;
-$D = $b*$b-4*$a+$c;
+$D = $b*$b-4*$a*$c;
 echo $D;
 if ($D > 0) {
-    echo 'два корня';
+    echo 'два корня' . "<br>";
+    echo "<sub>1</sub> =" . ($b-sqrt($D))/(2*$a) . "<br>";
+    echo "<sub>2</sub> =" . ($b+sqrt($D))/(2*$a) . "<br>";
 }elseif ($D < 0) {
     echo 'нет корней';
 }else{
-    echo 'один корень';
+    echo 'один корень' . "<br>";
+    echo "<sub>1</sub> =" . ($b-sqrt($D))/(2*$a) . "<br>";
 }
