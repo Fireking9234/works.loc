54f3daeb61fb7c0769c40cf7382d86f10267e2ab
DESKTOP-NQHV1GS\admin <mishakovalevst@gmail.com>
2023-03-27 14:06:46 +0300

    ЧАСЫ

--- lekzii/#7.php
+++ lekzii/#7.php
@@ -12,3 +12,12 @@ function day ($b){
     echo $b." Дни <br>" . $f . " Часы<br> ";
 }
 day(3);
+
+function nedel($b){
+    $i = 1;
+    while ($i <= 10):
+        echo $i * 24 . "<br>";
+        $i++;
+    endwhile;
+}
+nedel(1);
